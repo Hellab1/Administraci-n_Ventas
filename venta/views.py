15ba241dd886cc01457e06fafa7f4250d18d1787
Juan Espinoza <juan.espinoza@mamullapps.cl>
2021-08-30 16:11:18 -0400

    Mejora Autenticacón

--- venta/views.py
+++ venta/views.py
@@ -12,109 +12,118 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin  
 # Habilitamos los formularios en Django
 from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class ClienteListado(ListView): 
+class ClienteListado(LoginRequiredMixin, ListView): 
     model = Cliente # Llamamos a la clase 'Cliente' que se encuentra en nuestro archivo 'models.py'
-
-class ClienteCrear(SuccessMessageMixin, CreateView): 
+    login_url = '/iniciar-sesion/'
+class ClienteCrear(LoginRequiredMixin, SuccessMessageMixin, CreateView): 
     model = Cliente # Llamamos a la clase 'Cliente' que se encuentra en nuestro archivo 'models.py'
     form = Cliente # Definimos nuestro formulario con el nombre de la clase o modelo 'Cliente'
     fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'Cliente' de nuestra Base de Datos 
     success_message = 'Cliente Creado Correctamente !' # Mostramos este Mensaje luego de Crear un Cliente
- 
+    login_url = '/iniciar-sesion/'
     # Redireccionamos a la página principal luego de crear un registro o Cliente
     def get_success_url(self):        
         return reverse('leer_cliente') # Redireccionamos a la vista principal 'leer'
 
-class ClienteDetalle(DetailView): 
+class ClienteDetalle(LoginRequiredMixin, DetailView): 
     model = Cliente # Llamamos a la clase 'Cliente' que se encuentra en nuestro archivo 'models.py'
+    login_url = '/iniciar-sesion/'
 
-class ClienteActualizar(SuccessMessageMixin, UpdateView): 
+class ClienteActualizar(LoginRequiredMixin, SuccessMessageMixin, UpdateView): 
     model = Cliente # Llamamos a la clase 'Cliente' que se encuentra en nuestro archivo 'models.py' 
     form = Cliente # Definimos nuestro formulario con el nombre de la clase o modelo 'Cliente' 
     fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'Cliente' de nuestra Base de Datos 
     success_message = 'Cliente Actualizado Correctamente !' # Mostramos este Mensaje luego de Editar un Cliente 
- 
+    login_url = '/iniciar-sesion/' 
     # Redireccionamos a la página principal luego de actualizar un registro o Cliente
     def get_success_url(self):               
         return reverse('leer_cliente') # Redireccionamos a la vista principal 'leer'
 
-class ClienteEliminar(SuccessMessageMixin, DeleteView): 
+class ClienteEliminar(LoginRequiredMixin, SuccessMessageMixin, DeleteView): 
     model = Cliente 
     form = Cliente
     fields = "__all__"     
- 
+    login_url = '/iniciar-sesion/' 
     # Redireccionamos a la página principal luego de eliminar un registro o Cliente
     def get_success_url(self): 
         success_message = 'Cliente Eliminado Correctamente !' # Mostramos este Mensaje luego de Editar un Cliente 
         messages.success (self.request, (success_message))       
         return reverse('leer_cliente') # Redireccionamos a la vista principal 'leer'
 
-class ProductoListado(ListView): 
+class ProductoListado(LoginRequiredMixin, ListView): 
     model = Producto
+    login_url = '/iniciar-sesion/'
 
-class ProductoCrear(SuccessMessageMixin, CreateView): 
+class ProductoCrear(LoginRequiredMixin, SuccessMessageMixin, CreateView): 
     model = Producto
     form = Producto
     fields = "__all__" 
     success_message = 'Producto Creado Correctamente !' 
-
+    login_url = '/iniciar-sesion/'
     def get_success_url(self):        
         return reverse('leer_producto') 
 
-class ProductoDetalle(DetailView): 
+class ProductoDetalle(LoginRequiredMixin, DetailView): 
     model = Producto 
+    login_url = '/iniciar-sesion/'
 
-class ProductoActualizar(SuccessMessageMixin, UpdateView): 
+class ProductoActualizar(LoginRequiredMixin, SuccessMessageMixin, UpdateView): 
     model = Producto 
     form = Producto
     fields = "__all__"
     success_message = 'Producto Actualizado Correctamente !' 
-
+    login_url = '/iniciar-sesion/'
     def get_success_url(self):               
         return reverse('leer_producto') 
 
-class ProductoEliminar(SuccessMessageMixin, DeleteView): 
+class ProductoEliminar(LoginRequiredMixin, SuccessMessageMixin, DeleteView): 
     model = Producto 
     form = Producto
     fields = "__all__"     
- 
+    login_url = '/iniciar-sesion/'
     def get_success_url(self): 
         success_message = 'Producto Eliminado Correctamente !'
         messages.success (self.request, (success_message))       
         return reverse('leer_producto') 
 
-class OTListado(ListView): 
+class OTListado(LoginRequiredMixin, ListView): 
     model = Orden_Compra
+    login_url = '/iniciar-sesion/'
 
-class OTCrear(SuccessMessageMixin, CreateView): 
+class OTCrear(LoginRequiredMixin, SuccessMessageMixin, CreateView): 
     model = Orden_Compra
     form = Orden_Compra
     fields = "__all__" 
     success_message = 'Orden de Compra Creada Correctamente !' 
-
+    login_url = '/iniciar-sesion/'
     def get_success_url(self):        
         return reverse('leer_ot') 
 
-class OTDetalle(DetailView): 
+class OTDetalle(LoginRequiredMixin, DetailView): 
     model = Orden_Compra 
+    login_url = '/iniciar-sesion/'
 
-class OTActualizar(SuccessMessageMixin, UpdateView): 
+class OTActualizar(LoginRequiredMixin, SuccessMessageMixin, UpdateView): 
     model = Orden_Compra 
     form = Orden_Compra
     fields = "__all__"
     success_message = 'Orden de Compra Actualizada Correctamente !' 
-
+    login_url = '/iniciar-sesion/'
     def get_success_url(self):               
         return reverse('leer_ot') 
 
-class OTEliminar(SuccessMessageMixin, DeleteView): 
+class OTEliminar(LoginRequiredMixin, SuccessMessageMixin, DeleteView): 
     model = Orden_Compra 
     form = Orden_Compra
-    fields = "__all__"     
- 
+    fields = "__all__"   
+    login_url = '/iniciar-sesion/' 
     def get_success_url(self): 
         success_message = 'Orden de Compra Eliminada Correctamente !'
         messages.success (self.request, (success_message))       
         return reverse('leer_ot') 
+
+def ventas():
+    'hola mundo'
